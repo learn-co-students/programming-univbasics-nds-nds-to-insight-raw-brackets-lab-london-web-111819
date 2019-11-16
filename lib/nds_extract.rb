34b8dc4fd377ba30c5990ad191d2793e48f981a0
keemo <keemowatson1@gmail.com>
2019-11-16 22:54:36 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,76 +3,48 @@ require 'directors_database'
 require "pry"
 def directors_totals(nds)
 
-  # result = {
-  # }
-  # countForDirectorsNames = 0
-  #    countforfilmgross = 0
-     #result[directors_database[countForDirectorsNames][:name]] += directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross]
-#p  result[directors_database[countForDirectorsNames][:name]] += directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross]
 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
 
-    # puts pp directors_database#[0][:movies][1][:worldwide_gross]
-
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
 
 
-    # result[directors_database[countForDirectorsNames][:name]] = directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross]
-    #  result[directors_database[countForDirectorsNames][:name]] += directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross]
-    # puts result
-    # while countForDirectorsNames < directors_database.count
-    #   # result = (directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross])
-
     # puts directors_database[8]
-    result = {}
-    directors_database.each do |director|
-      gross = 0
-      director[:movies].each do |movie|
-      gross += movie[:worldwide_gross]
-      end
-      result[director[:name]] = gross
-    end
-
-      result
-
-
-
-# pp directors_database
-
-
-
-    # while countforfilmgross < directors_database[countForDirectorsNames][:movies].count
-    #    if result[directors_database[countForDirectorsNames][:name]]
-    #       result[directors_database[countForDirectorsNames][:name]] += (directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross])
-    #       countforfilmgross += 1
-    #     else
-    #     result[directors_database[countForDirectorsNames][:name]] = (directors_database[countForDirectorsNames][:movies][countforfilmgross][:worldwide_gross])
-    #    countforfilmgross += 1
-    #  end
-    # pry
-    #  if countForDirectorsNames != directors_database[countForDirectorsNames].count
-    #    if countforfilmgross == directors_database[countForDirectorsNames][:movies].count
-    #      countforfilmgross = 0
-    #      countForDirectorsNames += 1
-    #    end
-    #    puts result
-    #    end
-    #
-    #    return
-    #  end
-
-
-
-
-     # if directors_database[countForDirectorsNames][:movies] = nil
-     #   countforfilmgross = 0 countForDirectorsNames += 1
-    #  puts result
-     #     if directors_database[countForDirectorsNames][:movies][countforfilmgross] > directors_database[countForDirectorsNames][:movies].count
-     #   countForDirectorsNames += 1
-    #
+    # result = {}
+    # directors_database.each do |director|
+    #   gross = 0
+    #   director[:movies].each do |movie|
+    #   gross += movie[:worldwide_gross]
+    #   end
+    #   result[director[:name]] = gross
     # end
-# result
+    #   result
+
+
+    directorCount = 0
+    grossCount = 0
+    result = {}
+    while directorCount < directors_database.count
+      if result[directors_database[directorCount][:name]]
+        result[directors_database[directorCount][:name]] += (directors_database[directorCount][:movies][grossCount][:worldwide_gross])
+       grossCount += 1
+       puts result
+     else
+      result[directors_database[directorCount][:name]] = (directors_database[directorCount][:movies][grossCount][:worldwide_gross])
+      grossCount += 1
+    end
+         while grossCount == directors_database[directorCount][:movies].count
+           grossCount = 0
+           directorCount += 1
+           if directorCount == directors_database.count
+             break
+         end
+       end
+     end
+     result
+
+
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
